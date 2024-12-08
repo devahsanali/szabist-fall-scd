0b5a9bfa1ddd7244911aaa07db26ea910ed21b93
ahsan <mahsanali_@hotmail.com>
2024-12-09 04:18:33 +0500

    feat: Installed Passport for API authentication, implemented auth login via API, and added middleware for Teacher module.

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,35 @@
 <?php
 
 use App\Http\Controllers\Api\TeacherApiController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::name('teacher.')->prefix('teacher')->group(function() {
-    Route::get('/', [TeacherApiController::class, 'index'])->name('index');
-    Route::get('create', [TeacherApiController::class, 'create'])->name('create');
-    Route::post('store', [TeacherApiController::class, 'store'])->name('store');
-    Route::get('edit/{id}', [TeacherApiController::class, 'edit'])->name('edit');
-    Route::post('update', [TeacherApiController::class, 'update'])->name('update');
-    Route::get('destroy/{id}', [TeacherApiController::class, 'destroy'])->name('destroy');
+// Public route for login
+Route::post('login', [AuthController::class, 'login']);
+
+// Protected route
+Route::middleware('auth:api')->group(function () {
+    // Other protected routes can go here
+    Route::get('/profile', function (Request $request) {
+        return response()->json([
+            'user' => $request->user(),
+        ]);
+    });
+
+    // Logout route (revoke the access token)
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::name('teacher.')->prefix('teacher')->group(function() {
+        Route::get('/', [TeacherApiController::class, 'index'])->name('index');
+        Route::get('create', [TeacherApiController::class, 'create'])->name('create');
+        Route::post('store', [TeacherApiController::class, 'store'])->name('store');
+        Route::get('edit/{id}', [TeacherApiController::class, 'edit'])->name('edit');
+        Route::post('update', [TeacherApiController::class, 'update'])->name('update');
+        Route::get('destroy/{id}', [TeacherApiController::class, 'destroy'])->name('destroy');
+    });
 });
+
+
+
+
